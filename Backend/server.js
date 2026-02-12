c5d7757dccee39d44bc6346a2673394faec4b34f
Msuhash <msuhashrdj@gmail.com>
2026-02-12 19:35:46 +0530

    backe

--- Backend/server.js
+++ Backend/server.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
     cors({
-        origin: [process.env.CLIENT_URL, "http://localhost:5174"],
+        origin: [process.env.CLIENT_URL, "http://localhost:5174", "https://expense-tracker-swart-eta.vercel.app"],
         methods: ["POST", "PUT", "DELETE", "GET"],
         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true
