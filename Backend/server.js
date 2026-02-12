9df4fb1992566ac682113f6cbd82c10683396ba6
Msuhash <msuhashrdj@gmail.com>
2026-02-12 19:44:18 +0530

    default API is fixed

--- Backend/server.js
+++ Backend/server.js
@@ -44,3 +44,7 @@ app.use("/api/budget", budgetRouter)
 app.use("/api/export", exportRouter)
 
 app.listen(PORT, () => { console.log(`server is running successfully ${PORT}`) })
+
+app.get("/", (req, res) => {
+  res.send("Expense Tracker API Running");
+});
