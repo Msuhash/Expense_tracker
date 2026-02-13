98ce932280519b66339cf4836aff85345896d123
Msuhash <msuhashrdj@gmail.com>
2026-02-13 15:26:32 +0530

    fixed

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,11 +1,11 @@
-import createRoot from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Store from './store/Store.js';
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
-    <Provider store = {Store}>
-      <App />
-    </Provider>
+  <Provider store={Store}>
+    <App />
+  </Provider>
 )
